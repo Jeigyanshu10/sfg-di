67fb47d8873c0705f55722e2260b60e061a0d33d
jeigyanshu10 <Jeigyanshu.sarangi@gmail.com>
2022-03-24 03:20:54 +0530

    Removed the datasource.properties file

--- src/main/java/com/anshu/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/anshu/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+
 @Configuration
 public class GreetingServiceConfig {
 
